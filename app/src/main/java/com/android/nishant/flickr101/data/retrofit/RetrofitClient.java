4ae5ed52a6df65673535791389ce416649ea7066
Nishant Patel <nishantuwindsor@gmail.com>
2018-09-07 19:03:00 -0400

    Add retrofit client

---
+++ app/src/main/java/com/android/nishant/flickr101/data/retrofit/RetrofitClient.java
@@ -0,0 +1,25 @@
+/*
+ * Copyright (c) 2018
+ * File Created by nishant
+ */
+
+package com.android.nishant.flickr101.data.retrofit;
+
+import retrofit2.Retrofit;
+
+/**
+ * Retrofit client SingleTon for network operation
+ */
+public class RetrofitClient {
+    private static Retrofit sRetrofit = null;
+    private static final String BASE_URL = "https://api.flickr.com/services/rest/";
+
+    public static Retrofit getClient() {
+        if (sRetrofit == null) {
+            sRetrofit = new Retrofit.Builder()
+                    .baseUrl(BASE_URL)
+                    .build();
+        }
+        return sRetrofit;
+    }
+}
