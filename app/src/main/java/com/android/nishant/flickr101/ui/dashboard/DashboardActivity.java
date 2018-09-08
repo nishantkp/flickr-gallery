430a3078e07c5d720acdc35c08a12cc9d641d73c
Nishant Patel <nishantuwindsor@gmail.com>
2018-09-08 14:18:41 -0400

    Hook RecyclerView with Adapter for Dashboard

--- app/src/main/java/com/android/nishant/flickr101/ui/dashboard/DashboardActivity.java
+++ app/src/main/java/com/android/nishant/flickr101/ui/dashboard/DashboardActivity.java
@@ -8,11 +8,14 @@ package com.android.nishant.flickr101.ui.dashboard;
 import android.databinding.DataBindingUtil;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.DividerItemDecoration;
+import android.support.v7.widget.LinearLayoutManager;
 import android.util.Log;
 
 import com.android.nishant.flickr101.databinding.ActivityDashboardBinding;
 import com.android.nishant.flickr101.R;
 import com.android.nishant.flickr101.data.manager.DataManager;
+import com.android.nishant.flickr101.ui.adapter.PhotoAdapter;
 import com.android.nishant.flickr101.ui.model.PhotoDetail;
 
 import java.util.List;
@@ -23,6 +26,7 @@ public class DashboardActivity
 
     private DashboardPresenter mPresenter;
     private ActivityDashboardBinding mBinding;
+    private PhotoAdapter mPhotoAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,6 +35,17 @@ public class DashboardActivity
 
         mPresenter = new DashboardPresenter(DataManager.getInstance());
         mPresenter.attachView(this);
+
+        mPhotoAdapter = new PhotoAdapter();
+        mBinding.dashboardRv.setAdapter(mPhotoAdapter);
+
+        // set layout manager on recycler view
+        mBinding.dashboardRv.setLayoutManager(new LinearLayoutManager(this));
+
+        // Add divider between two items
+        DividerItemDecoration itemDecor =
+                new DividerItemDecoration(mBinding.dashboardRv.getContext(), DividerItemDecoration.VERTICAL);
+        mBinding.dashboardRv.addItemDecoration(itemDecor);
 
         // Fake query
         mPresenter.getPhotos("bird");
@@ -48,9 +63,7 @@ public class DashboardActivity
 
     @Override
     public void onData(List<PhotoDetail> photoDetails) {
-        for (PhotoDetail photo : photoDetails) {
-            Log.i("Data", "\nTitle : " + photo.getTitle() + " Url : " + photo.getUrl());
-        }
+        mPhotoAdapter.updateDataSet(photoDetails);
     }
 
     @Override
