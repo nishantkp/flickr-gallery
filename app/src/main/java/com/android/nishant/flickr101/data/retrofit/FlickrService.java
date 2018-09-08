4fb0b35cf17c7ac16c76a07cb7ff1800fa8f3435
Nishant Patel <nishantuwindsor@gmail.com>
2018-09-08 17:58:06 -0400

    Add GET method for retrieving photo sizes from Flickr API

--- app/src/main/java/com/android/nishant/flickr101/data/retrofit/FlickrService.java
+++ app/src/main/java/com/android/nishant/flickr101/data/retrofit/FlickrService.java
@@ -6,6 +6,7 @@
 package com.android.nishant.flickr101.data.retrofit;
 
 import com.android.nishant.flickr101.ui.model.FlickrObject;
+import com.android.nishant.flickr101.ui.model.FlickrSizeQuery;
 
 import io.reactivex.Observable;
 import retrofit2.http.GET;
@@ -24,4 +25,14 @@ public interface FlickrService {
      */
     @GET("?api_key=949e98778755d1982f537d56236bbb42&method=flickr.photos.search&format=json")
     Observable<FlickrObject> getDataFromQuery(@Query("text") String searchQuery);
+
+    /**
+     * Retrofit API for GET method
+     * This method appends photoId to flicker end point ti get all available photo sizes
+     *
+     * @param photoId photo id
+     * @return FlickrSizeQuery object which contains all the available photo sizes for particular id
+     */
+    @GET("?api_key=949e98778755d1982f537d56236bbb42&method=flickr.photos.getSizes&format=json")
+    Observable<FlickrSizeQuery> getImageSizes(@Query("photo_id") String photoId);
 }
