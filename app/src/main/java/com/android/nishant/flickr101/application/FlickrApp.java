62360a946bacf849322bffe9f90fa1f3fb7df150
Nishant Patel <nishantuwindsor@gmail.com>
2018-09-10 18:58:33 -0400

    Make Application class implement ActivityInjector

--- app/src/main/java/com/android/nishant/flickr101/application/FlickrApp.java
+++ app/src/main/java/com/android/nishant/flickr101/application/FlickrApp.java
@@ -5,19 +5,35 @@
 
 package com.android.nishant.flickr101.application;
 
+import android.app.Activity;
 import android.app.Application;
 
 import com.android.nishant.flickr101.data.manager.DataManager;
 
+import javax.inject.Inject;
+
+import dagger.android.AndroidInjector;
+import dagger.android.DispatchingAndroidInjector;
+import dagger.android.HasActivityInjector;
+
 /**
  * Application class
  */
-public class FlickrApp extends Application {
+public class FlickrApp extends Application implements HasActivityInjector {
+
+    @Inject
+    DispatchingAndroidInjector<Activity> mActivityDispatchingAndroidInjector;
+
     @Override
     public void onCreate() {
         super.onCreate();
 
         // Initialize dataManager when app starts
         DataManager.getInstance();
+    }
+
+    @Override
+    public AndroidInjector<Activity> activityInjector() {
+        return mActivityDispatchingAndroidInjector;
     }
 }
